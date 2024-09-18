df39485a2086e3a9eb3fff6502b602fd3958f320
Abhilash <abhilasht0007.t@gmail.com>
2024-09-18 07:03:42 +0530

    Practise...

--- src/app.js
+++ src/app.js
@@ -6,26 +6,23 @@ app.delete("/user", (req, res) => {
   res.send("User data deleted successfully!");
 });
 
-app.get("/user", (req, res) => {
-  res.send({
-    firstName: "Abhilash",
-    lastName: "Tengli",
-  });
-});
-
-app.get("/user/:userId", (req, res) => {
-  console.log(req.params);
-  res.send("Executed successfully!");
-});
-
-app.get("/userQuery", (req, res) => {
-  console.log(req.query);
-  res.send("Executed successfully!");
-});
-
-app.post("/user", (req, res) => {
-  res.send("Data saved successfully!");
-});
+app.get(
+  "/user",
+  (req, res, next) => {
+    // res.send({
+    //   firstName: "Abhilash",
+    //   lastName: "Tengli",
+    // });
+    next();
+  },
+  (req, res, next) => {
+    // res.send("2nd Response executed");
+    next();
+  },
+  (req, res) => {
+    res.send("3rd Response executed");
+  }
+);
 
 app.listen(4000, () => {
   console.log("Server is listening on port 4000");
