f5a00d26c6cb0658f0c7fae16bf051eb1ab23d35
Abhilash <abhilasht0007.t@gmail.com>
2024-09-19 08:02:26 +0530

    Added Data Validation

--- src/app.js
+++ src/app.js
@@ -35,6 +35,8 @@ app.get("/user", async (req, res) => {
 app.get("/feed", async (req, res) => {
   try {
     const users = await User.find({});
+    console.log(userSkils);
+
     res.send(users);
   } catch (err) {
     res.status(400).send("Something went wrong");
@@ -58,13 +60,57 @@ app.delete("/user", async (req, res) => {
   }
 });
 
-app.put("/user", async (req, res) => {
-  const userId = req.body.userId;
+app.put("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
   const data = req.body;
 
   try {
-    await User.findByIdAndUpdate({ _id: userId }, data);
+    const userSkills = await User.findById(userId, "skills");
+    // console.log(userSkills.skills);
+
+    const ALLOWED_UPDATES = [
+      "photoUrl",
+      "age",
+      "gender",
+      "skills",
+      "about",
+      "firstName",
+      "lastName",
+    ];
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+    if (!isUpdateAllowed) {
+      throw new Error("Updates are not allowed");
+    }
+    const isSkillPresent = data.skills.some((k) =>
+      userSkills.skills.includes(k)
+    );
+    if (isSkillPresent) {
+      throw new Error("Some of the Skills already exist");
+    }
+    await User.findByIdAndUpdate({ _id: userId }, data, {
+      runValidators: true,
+    });
     res.send("User Updated successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong " + err.message);
+  }
+});
+
+app.put("/updateUserByEmail", async (req, res) => {
+  const email = req.body.emailId;
+  const data = req.body;
+
+  try {
+    const user = await User.updateOne({ emailId: email }, data, {
+      runValidators: true,
+    });
+
+    if (user.matchedCount === 0) {
+      res.status(404).send("User not found with email " + emailId);
+    }
+    res.send("User updated successfully by emailId");
   } catch (err) {
     res.status(400).send("Something went wrong");
   }
@@ -73,6 +119,7 @@ app.put("/user", async (req, res) => {
 connectDB()
   .then(() => {
     console.log("Database connection established");
+
     app.listen(4000, () => {
       console.log("Server is listening on port 4000");
     });
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,60 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+      minLenght: 2,
+      maxLenght: 20,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+      minLenght: 2,
+      maxLenght: 20,
+    },
+    emailId: {
+      type: String,
+      lowercase: true,
+      required: true,
+      unique: true,
+      trim: true,
+      email: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLenght: 8,
+    },
+    age: {
+      type: Number,
+      min: 18,
+      max: 130,
+      minLength: 1,
+      maxLenght: 3,
+    },
+    skills: {
+      type: [String],
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Invalid gender");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+    },
+    about: {
+      type: String,
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
