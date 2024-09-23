30c7fcbe890894b345fdc5f535e76dcd02fb77aa
Abhilash <abhilasht0007.t@gmail.com>
2024-09-23 07:56:43 +0530

    Request API Implementation

--- src/routes/request.js
+++ src/routes/request.js
@@ -3,6 +3,7 @@ const requestRouter = express.Router();
 const { userAuth } = require("../middlerware/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 const User = require("../models/user");
+const { Connection } = require("mongoose");
 
 requestRouter.post(
   "/request/send/:status/:toUserId",
@@ -61,6 +62,49 @@ requestRouter.post(
             ? `${req.user.firstName} is interested in ${toUser.firstName}`
             : `${req.user.firstName} ignored ${toUser.firstName}`
         }`,
+        data,
+      });
+    } catch (err) {
+      res.status(400).send("ERROR : " + err.message);
+    }
+  }
+);
+
+requestRouter.post(
+  "/request/review/:status/:requestId",
+  userAuth,
+  async (req, res) => {
+    //LoggedInUser == toUserId
+    //check weather existing status == interested
+    //check incoming status is either accepted or rejected
+    //_id == requestId
+
+    try {
+      const loggedInUser = req.user;
+      const { status, requestId } = req.params;
+
+      const allowedStatus = ["accepted", "rejected"];
+      if (!allowedStatus.includes(status)) {
+        throw new Error("status not allowed");
+      }
+      const connectionRequest = await ConnectionRequest.findOne({
+        _id: requestId,
+        toUserId: loggedInUser._id,
+        status: "interested",
+      });
+
+      if (!connectionRequest) {
+        return res.status(400).json({
+          message: "Connection request not found",
+        });
+      }
+
+      connectionRequest.status = status;
+
+      const data = await connectionRequest.save();
+
+      res.json({
+        message: "Connection request " + status,
         data,
       });
     } catch (err) {
