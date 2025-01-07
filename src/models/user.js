b122c8e06113a83f3631495c8dbf5e96115f32c8
Abhilash <abhilasht0007.t@gmail.com>
2025-01-07 20:58:25 +0530

    update user schema typo error

--- src/models/user.js
+++ src/models/user.js
@@ -9,14 +9,14 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
       trim: true,
-      minLenght: 2,
-      maxLenght: 20,
+      minLength: 2,
+      minLength: 20
     },
     lastName: {
       type: String,
       trim: true,
-      minLenght: 2,
-      maxLenght: 20,
+      minLength: 2,
+      minLength: 20
     },
     emailId: {
       type: String,
@@ -29,34 +29,34 @@ const userSchema = new mongoose.Schema(
         if (!validator.isEmail(value)) {
           throw new Error("Invalid email address");
         }
-      },
+      }
     },
     password: {
       type: String,
       required: true,
-      minLenght: 8,
+      minLength: 8,
       validate(value) {
         if (!validator.isStrongPassword(value)) {
           throw new Error("Enter a Strong Password");
         }
-      },
+      }
     },
     age: {
       type: Number,
       min: 18,
       max: 130,
       minLength: 1,
-      maxLenght: 3,
+      minLength: 3
     },
     skills: {
-      type: [String],
+      type: [String]
     },
     gender: {
       type: String,
       enum: {
         values: ["male", "female", "other"],
-        message: `{VALUE} is invalid gender`,
-      },
+        message: `{VALUE} is invalid gender`
+      }
       // validate(value) {
       //   if (!["male", "female", "others"].includes(value)) {
       //     throw new Error("Invalid gender");
@@ -71,11 +71,11 @@ const userSchema = new mongoose.Schema(
         if (!validator.isURL(value)) {
           throw new Error("Invalid photo URL : " + value);
         }
-      },
+      }
     },
     about: {
-      type: String,
-    },
+      type: String
+    }
   },
   { timestamps: true }
 );
